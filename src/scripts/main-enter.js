baf71326ad83d3baad083dc7bb6b76ac5e2f6e46
abezuska <abezuska@gmail.com>
2017-05-23 21:11:23 -0400

    setup game size

--- src/scripts/main-enter.js
+++ src/scripts/main-enter.js
@@ -1,5 +1,6 @@
 "use strict";
 
 module.exports = function(game) { // eslint-disable-line no-unused-vars
+  game.scaleCanvasToFitRectangle(640, 1136);
   game.prefabs.instantiate(game.entities, "bg");
 };
